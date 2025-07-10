95e2c3573e8342ed40eea6b9f910943aae41bca5
trev <me@trev.xyz>
2025-07-10 17:44:39 -0400

    revert: remove opengrep

--- default.nix
+++ default.nix
@@ -1,18 +1,12 @@
 {
   system ? builtins.currentSystem,
   pkgs ? import <nixpkgs> {inherit system;},
-}: rec {
+}: {
   lib = import ./lib {inherit pkgs;}; # functions
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  # libraries
-  opam-nix = import ./libs/opam-nix/src/opam.nix {
-    inherit pkgs;
-  };
-
   # packages
   bobgen = pkgs.callPackage ./pkgs/bobgen {};
   protoc-gen-connect-openapi = pkgs.callPackage ./pkgs/protoc-gen-connect-openapi {};
-  opengrep = pkgs.callPackage ./pkgs/opengrep {inherit pkgs opam-nix;};
 }
--- pkgs/opengrep/default.nix
+++
@@ -1,7 +0,0 @@
-{
-  pkgs,
-  opam-nix,
-}: let
-  opengrep = import ./opengrep.nix {inherit pkgs opam-nix;};
-in
-  import ./pyopengrep.nix {inherit pkgs opengrep;}
