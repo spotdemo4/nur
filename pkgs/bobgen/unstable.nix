48aba9682a1e4f3e1aec56ad77b34a23fc300e5d
trev <me@trev.xyz>
2025-10-01 02:26:20 -0400

    bobgen.unstable: 0.41.1-unstable-2025-09-02 -> 0.41.1-unstable-2025-09-27
    Diff: https://github.com/stephenafamo/bob/compare/10b9d8cfbc4b534386f6db8ed02ea61d1fc34726...9d3b4c4b34a4263a9d9a42d1ed92e3aa6805ea30
    
    Changelog: https://github.com/stephenafamo/bob/commits/9d3b4c4b34a4263a9d9a42d1ed92e3aa6805ea30/

--- pkgs/bobgen/unstable.nix
+++ pkgs/bobgen/unstable.nix
@@ -7,13 +7,13 @@
 }:
 bobgen.overrideAttrs
 (final: prev: {
-  version = "0.41.1-unstable-2025-09-02";
+  version = "0.41.1-unstable-2025-09-27";
 
   src = fetchFromGitHub {
     owner = "stephenafamo";
     repo = "bob";
-    rev = "10b9d8cfbc4b534386f6db8ed02ea61d1fc34726";
-    hash = "sha256-HaWeiC1sxJz2wWYsvopD8ufFZlpMiA1GkfzrnDvP1XM=";
+    rev = "9d3b4c4b34a4263a9d9a42d1ed92e3aa6805ea30";
+    hash = "sha256-+WEJlvjw385Bh7ODol54ThxB1g6M2JwcJr8UJ4ssy78=";
   };
 
   vendorHash = "sha256-Jqlah37+tfNqsgeL/MnbVUmSfU2JWMJDb9AQrEqXnXU=";
