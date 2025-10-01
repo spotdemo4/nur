eedaa00b73bb8ad2205d24ccc0fafea38dffe87e
trev <me@trev.xyz>
2025-10-01 02:27:56 -0400

    style: format

--- update.nix
+++ update.nix
@@ -41,12 +41,14 @@
     attrs;
 
   total = (lib.attrsets.collect lib.isDerivation topDerivations) ++ (lib.attrsets.collect lib.isDerivation subDerivations);
-  commands = map (d: ''
-    echo "updating ${d.pname}..."
-    {
-    ${d.updateScript}
-    } || echo "failed to update ${d.pname}"
-  '') total;
+  commands =
+    map (d: ''
+      echo "updating ${d.pname}..."
+      {
+      ${d.updateScript}
+      } || echo "failed to update ${d.pname}"
+    '')
+    total;
 in
   writeShellApplication {
     name = "update";
