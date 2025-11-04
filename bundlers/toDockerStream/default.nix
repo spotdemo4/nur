eb724233f481bd3d58421119da508eb6f5f7e2db
trev <me@trev.xyz>
2025-11-04 00:02:28 -0500

    fix: add caCertificates by default

--- bundlers/toDockerStream/default.nix
+++ bundlers/toDockerStream/default.nix
@@ -6,7 +6,10 @@ pkgs.dockerTools.streamLayeredImage {
   name = "${drv.pname}";
   tag = "${drv.version}";
   created = "now";
-  contents = [drv];
+  contents = [
+    drv
+    pkgs.dockerTools.caCertificates
+  ];
   config = {
     Cmd = [
       "${pkgs.lib.meta.getExe drv}"
