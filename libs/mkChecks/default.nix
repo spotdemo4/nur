ffde47273130fa7e9959ea97dffb93b5d129a045
trev <me@trev.xyz>
2025-11-17 13:32:21 -0500

    style: nixfmt

--- libs/mkChecks/default.nix
+++ libs/mkChecks/default.nix
@@ -1,12 +1,14 @@
-{pkgs}: let
+{ pkgs }:
+let
   isDerivation = p: builtins.isAttrs p && p ? type && p.type == "derivation";
 in
-  builtins.mapAttrs (name: check:
-    if isDerivation check.src
-    then
-      check.src.overrideAttrs (final: prev: {
+builtins.mapAttrs (
+  name: check:
+  if isDerivation check.src then
+    check.src.overrideAttrs (
+      final: prev: {
         name = name;
-        nativeBuildInputs = prev.nativeBuildInputs ++ (check.deps or check.nativeBuildInputs or []);
+        nativeBuildInputs = prev.nativeBuildInputs ++ (check.deps or check.nativeBuildInputs or [ ]);
 
         dontBuild = true;
 
@@ -21,25 +23,27 @@ in
         '';
 
         dontFixup = true;
-      })
-    else
-      pkgs.stdenvNoCC.mkDerivation {
-        name = name;
-        src = check.src or ./.;
-        nativeBuildInputs = check.deps or check.nativeBuildInputs or [];
+      }
+    )
+  else
+    pkgs.stdenvNoCC.mkDerivation {
+      name = name;
+      src = check.src or ./.;
+      nativeBuildInputs = check.deps or check.nativeBuildInputs or [ ];
 
-        dontConfigure = true;
-        dontBuild = true;
+      dontConfigure = true;
+      dontBuild = true;
 
-        doCheck = true;
-        checkPhase = pkgs.lib.strings.concatLines [
-          "export HOME=$(mktemp -d)"
-          check.script or check.checkPhase
-        ];
+      doCheck = true;
+      checkPhase = pkgs.lib.strings.concatLines [
+        "export HOME=$(mktemp -d)"
+        check.script or check.checkPhase
+      ];
 
-        installPhase = ''
-          touch $out
-        '';
+      installPhase = ''
+        touch $out
+      '';
 
-        dontFixup = true;
-      })
+      dontFixup = true;
+    }
+)
