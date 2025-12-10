6ec9582b50ebc46baf7002c8798362cdcd9c6f87
spotdemo4 <me@trev.xyz>
2025-12-10 10:31:29 -0500

    feat: renovate flake updates

--- packages/bumper/default.nix
+++ packages/bumper/default.nix
@@ -1,5 +1,5 @@
 {
   system,
 }:
-(builtins.getFlake "github:spotdemo4/bumper/0fb83bdfb2cd3d781925e73d5bbc79b413666263")
-.packages."${system}".default
+(builtins.getFlake "github:spotdemo4/bumper/dec2fb0d577c332bc244acfb6bd35ffc88703c21" # v0.4.5
+).packages."${system}".default
--- packages/nix-fix-hash/default.nix
+++ packages/nix-fix-hash/default.nix
@@ -1,5 +1,5 @@
 {
   system,
 }:
-(builtins.getFlake "github:spotdemo4/nix-fix-hash/03e7eacb639a1b87ab9beb05defa784725bb2b57")
-.packages."${system}".default
+(builtins.getFlake "github:spotdemo4/nix-fix-hash/9d7e396cd940e50a7ff57d97e4fcc6eeab8b7b19" # v0.1.1
+).packages."${system}".default
