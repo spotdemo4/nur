a40fa0527f4af1a3f3f8d69cf4c8b854a616a772
spotdemo4 <me@trev.xyz>
2025-12-14 15:39:18 -0500

    feat: nix-scan

---
+++ packages/nix-scan/default.nix
@@ -0,0 +1,63 @@
+{
+  jq,
+  lib,
+  makeWrapper,
+  ncurses,
+  nix,
+  pcre2,
+  runtimeShell,
+  shellcheck,
+  stdenv,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "nix-scan";
+  version = "1.1.1";
+
+  src = builtins.path {
+    name = "root";
+    path = ./.;
+  };
+
+  nativeBuildInputs = [
+    makeWrapper
+    shellcheck
+  ];
+
+  runtimeInputs = [
+    jq
+    ncurses
+    nix
+    pcre2
+  ];
+
+  unpackPhase = ''
+    cp -a "$src/nix-scan.sh" nix-scan.sh
+  '';
+
+  dontBuild = true;
+
+  configurePhase = ''
+    sed -i '1c\#!${runtimeShell}' nix-scan.sh
+    sed -i '2c\export PATH="${lib.makeBinPath finalAttrs.runtimeInputs}:$PATH"' nix-scan.sh
+  '';
+
+  doCheck = true;
+  checkPhase = ''
+    shellcheck nix-scan.sh
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp nix-scan.sh "$out/bin/nix-scan"
+  '';
+
+  dontFixup = true;
+
+  meta = {
+    description = "Nix vulnerability scanner";
+    mainProgram = "nix-scan";
+    homepage = "https://github.com/spotdemo4/nix-scan";
+    changelog = "https://github.com/spotdemo4/nix-scan/releases/tag/v${finalAttrs.version}";
+    platforms = lib.platforms.all;
+  };
+})
