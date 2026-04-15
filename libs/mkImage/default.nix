240bd99c364cd50b00b6dc9ee537d53af6af9ba8
spotdemo4 <me@trev.xyz>
2026-04-15 05:02:26 -0400

    fix: try to use alternate system

--- libs/mkImage/default.nix
+++ libs/mkImage/default.nix
@@ -12,7 +12,14 @@
 assert src != null;
 
 let
-  package = if stdenv.hostPlatform.isStatic then (src.${stdenv.hostPlatform.system} or src) else src;
+  package =
+    if
+      (stdenv.hostPlatform.isStatic || (stdenv.hostPlatform.system != stdenv.buildPlatform.system))
+    then
+      (src.${stdenv.hostPlatform.system} or src)
+    else
+      src;
+
   platforms = [
     "x86_64-linux"
     "x86_64-linux-musl"
