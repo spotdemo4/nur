97903ddb5932adae4f45f5cfc16592e20e385aa7
trev <me@trev.xyz>
2026-04-28 17:44:30 -0400

    fix(schemas): escape dollar sign

--- schemas/pkgs.nix
+++ schemas/pkgs.nix
@@ -4,7 +4,7 @@
   doc = ''
     The `pkgs` flake output contains the modified nixpkgs package set used by the project.
     Primarily consumed by nixd to provide package & lib completion/information from it:
-    `(builtins.getFlake (builtins.toString ./.)).outputs.pkgs.${builtins.currentSystem}`
+    `(builtins.getFlake (builtins.toString ./.)).outputs.pkgs.''${builtins.currentSystem}`
   '';
   appendSystem = true;
   defaultAttrPath = [ ];
