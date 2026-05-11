0d4540e7ab9263f4191f30976d4178d5edeebf20
trev <me@trev.xyz>
2026-05-11 05:04:00 -0400

    fix(mkApps): just passthru derivations

--- libs/mkApps/default.nix
+++ libs/mkApps/default.nix
@@ -1,34 +1,38 @@
 {
-  stdenvNoCC,
+  lib,
   replaceVars,
+  stdenvNoCC,
 }:
 
 builtins.mapAttrs (
   name: script:
-  let
-    program = stdenvNoCC.mkDerivation (finalAttrs: {
-      inherit name;
+  if lib.isDerivation script then
+    script
+  else
+    let
+      program = stdenvNoCC.mkDerivation (finalAttrs: {
+        inherit name;
 
-      app = replaceVars ./app.sh {
+        app = replaceVars ./app.sh {
+          inherit script;
+        };
+
+        dontUnpack = true;
+        dontConfigure = true;
+        dontBuild = true;
+        doCheck = false;
+
+        installPhase = ''
+          install -D ${finalAttrs.app} $out/bin/${name}
+        '';
+      });
+    in
+    {
+      type = "app";
+      program = "${program}/bin/${name}";
+      meta = {
         inherit script;
+        description = script;
       };
-
-      dontUnpack = true;
-      dontConfigure = true;
-      dontBuild = true;
-      doCheck = false;
-
-      installPhase = ''
-        install -D ${finalAttrs.app} $out/bin/${name}
-      '';
-    });
-  in
-  {
-    type = "app";
-    program = "${program}/bin/${name}";
-    meta = {
-      inherit script;
-      description = script;
-    };
-  }
+    }
 )
